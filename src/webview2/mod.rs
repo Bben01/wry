a77549c97401f7b2a069c60b80ff6b8591f8e0bd
Bben01 <52465698+Bben01@users.noreply.github.com>
2025-07-18 10:01:32 +0300

    Rename ipc handler

--- src/webview2/mod.rs
+++ src/webview2/mod.rs
@@ -796,7 +796,7 @@ impl InnerWebView {
     Self::add_script_to_execute_on_document_created(
       webview,
       String::from(
-        r#"Object.defineProperty(window, 'ipc', { value: Object.freeze({ postMessage: s=> window.chrome.webview.postMessage(s) }) });"#,
+        r#"Object.defineProperty(window, 'ipc_handler', { value: Object.freeze({ postMessage: s=> window.chrome.webview.postMessage(s) }) });"#,
       ),
     )?;
 
